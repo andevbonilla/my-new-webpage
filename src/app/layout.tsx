e77300e0e4b83a7ecf0e0629f555475c739c111f
Andres Bonilla <andresbonilla1256@gmail.com>
2023-11-10 12:20:56 -0500

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: { 
+    default: 'Andres Bonilla',
+    template: '%s | Andres Bonilla'
+  },
+  description: "Hello, my name is Andres Bonilla. I am a software developer and entrepreneur and this is my website",
+  keywords: ["andres", "bonilla", "andres bonilla", "andres camilo bonilla", "andreb"],
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"],
+    
+  },
+  manifest: "/site.webmanifest"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
