d61ef9114928e0a15b442111edadf1d73b22a557
Andres Bonilla <andresbonilla1256@gmail.com>
2023-11-12 23:26:00 -0500

    all the animation of vehicle  button finished

--- src/components/Vehicle.tsx
+++ src/components/Vehicle.tsx
@@ -4,85 +4,75 @@ import React, {useEffect, useState} from 'react';
 
 export const Vehicle = () => {
 
-    const [counterTime, setCounterTime] = useState(15); // in seconds
+    const [counterTime, setCounterTime] = useState(1); // in seconds
     const [vehicleIndex, setvehicleIndex] = useState(0);
     const [canUptade, setCanUptade] = useState(false);
+    
     const [vehicles, setVehicles] = useState([
         {
             name: "supercross",
             velocity: "35km/h",
-            selected: true,
             img: "vehicle1.png"
         },
         {
             name: "Honda cb 125f",
             velocity: "90km/h",
-            selected: false,
-            img: "vehicle1.png"
+            img: "vehicle2.png"
         },
         {
             name: "Renault Sandero",
             velocity: "160km/h",
-            selected: false,
-            img: "vehicle1.png"
+            img: "vehicle3.png"
         },
         {
             name: "BMW M4",
             velocity: "250km/h",
-            selected: false,
-            img: "vehicle1.png"
+            img: "vehicle4.png"
         },
         {
-            name: "Porsche 911 GT3",
+            name: "Porsche 992 GT3 rs",
             velocity: "320km/h",
-            selected: false,
-            img: "vehicle1.png"
+            img: "vehicle5.png"
         },
         {
             name: "Bugatti Chiron",
             velocity: "490km/h",
-            selected: false,
-            img: "vehicle1.png"
+            img: "vehicle6.png"
         },
         {
             name: "Boeing 737",
             velocity: "840km/h",
-            selected: false,
-            img: "vehicle1.png"
+            img: "vehicle7.png"
         },
         {
             name: "f-15 eagle",
             velocity: "2.600km/h",
-            selected: false,
-            img: "vehicle1.png"
+            img: "vehicle8.png"
         },
         {
             name: "Saturn V",
             velocity: "64.500km/h",
-            selected: false,
-            img: "vehicle1.png"
+            img: "vehicle9.png"
         },
         {
             name: "Parker Solar Probe",
             velocity: "692,000 km/h",
-            selected: false,
-            img: "vehicle1.png"
+            img: "vehicle10.png"
         },
     ]);
 
 
 
     useEffect(() => {
-        let number = 15;
+
+        let number = 1;
         const newInterval = setInterval(() => {
-            console.log(number)
             if (!canUptade) {
-                console.log("trabajando")
                 number--;
                 setCounterTime(number);
                 if (number === 0) {
-                    setCounterTime(15);
-                    number = 15;
+                    setCounterTime(1);
+                    number = 1;
                     setCanUptade(true);
                  }
             }
@@ -101,6 +91,7 @@ export const Vehicle = () => {
 
     const updateVehicle = () => {
         if (!canUptade) return;
+        if (vehicleIndex === 9) return;
         setvehicleIndex(vehicleIndex+1);
         setCanUptade(false);
     }
@@ -108,22 +99,26 @@ export const Vehicle = () => {
   return (
     <div className='fixed bottom-0 right-0 mr-6 z-50 flex flex-col justify-center items-center'>
 
-            <div className={`rounded-full bg-white p-4 ${(canUptade) ? "border-green-500" : "border-gray-400"} border-4`}>
+            <div className={`rounded-full bg-white p-2 ${(canUptade) ? "border-green-500" : "border-gray-400"} border-4`}>
 
                 <Image 
                     src={require(`@/assets/${vehicles[vehicleIndex].img}`)}
                     alt="bicicross"
-                    className='w-[3rem] h-[3rem]'
+                    className='w-[4.5rem] h-[4.5rem]'
                 />
 
             </div>
 
             {
-                canUptade && <button onClick={updateVehicle} type='button' className='bg-green-500 py-2 px-3 text-white rounded-full -translate-y-8 text-lg'>Update</button>
+                (canUptade && vehicleIndex !== 9) && <button onClick={updateVehicle} type='button' className='bg-green-500 py-2 px-3 text-white rounded-full -translate-y-8 text-lg'>Update</button>
             }
 
             {
-                !canUptade && <button type='button' className='bg-gray-400 py-2 px-3 text-white rounded-full -translate-y-8 text-lg'>{counterTime}s</button>
+                (!canUptade && vehicleIndex !== 9) && <p className='bg-gray-400 py-2 px-3 text-white rounded-full -translate-y-8 text-lg'>{counterTime}s</p>
+            }
+
+            {
+                (vehicleIndex === 9) && <p className='opacity-0'>{counterTime}s</p>
             }
             
     </div>
