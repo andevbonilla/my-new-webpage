adf89b0e89b9c82697751726d04ed322ed8e819f
Andres Bonilla <andresbonilla1256@gmail.com>
2023-11-13 01:41:11 -0500

    can't click the button of update if scrolling

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -10,23 +10,16 @@ export const StarsBackground = ({children}:any) => {
                 const totalHeight = Math.max(
                     document.body.scrollHeight,
                     document.documentElement.scrollHeight
-                );
-
-                console.log(totalHeight * 0.50)
-                console.log(Math.round(window.scrollY))
-                console.log(Math.round(window.scrollY) > (totalHeight * 0.50));
-                
+                );                
                 if (Math.round(window.scrollY) > (totalHeight * 0.50)) {
                         // sin estrellas
-                        console.log("sdsdsd")
                         starElem.current.className = `no-stars`;                                       
                 }else {
                         // con estrellas
-                        console.log("000000")
                         starElem.current.className = `stars`;                    
-                }
+                };
 
-    }
+    };
 
     useEffect(() => {
 
--- src/components/Vehicle.tsx
+++ src/components/Vehicle.tsx
@@ -1,12 +1,18 @@
 "use client"
 import Image from 'next/image';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 
 export const Vehicle = () => {
 
-    const [counterTime, setCounterTime] = useState(1); // in seconds
+    const [counterTime, setCounterTime] = useState(15); // in seconds
     const [vehicleIndex, setvehicleIndex] = useState(0);
     const [canUptade, setCanUptade] = useState(false);
+    const [isGoingUp, setIsGoingUp] = useState(false);
+    const [canClick, setCanClick] = useState(true);
+    
+
+
+    const elemento:any = useRef(null);
     
     const [vehicles, setVehicles] = useState([
         {
@@ -65,14 +71,14 @@ export const Vehicle = () => {
 
     useEffect(() => {
 
-        let number = 1;
+        let number = 15;
         const newInterval = setInterval(() => {
             if (!canUptade) {
                 number--;
                 setCounterTime(number);
                 if (number === 0) {
-                    setCounterTime(1);
-                    number = 1;
+                    setCounterTime(15);
+                    number = 15;
                     setCanUptade(true);
                  }
             }
@@ -84,43 +90,100 @@ export const Vehicle = () => {
       
     }, [canUptade]);
 
+    let isScrolling:any;
+    document.addEventListener('scroll', function () {
+        
+        clearTimeout(isScrolling);
+        setIsGoingUp(false);
+        setCanClick(false);
 
+        if (elemento.current) {
+            elemento.current.style.opacity = '0';
+            elemento.current.classList.remove('opacity-transition');
+        }
+
+        isScrolling = setTimeout(function () {
+            if (elemento.current) {
+                elemento.current.classList.add('opacity-transition');
+                elemento.current.style.opacity = '1';
+            }
+            setCanClick(true);
+        }, 200);
+
+    }, false);
     
-
     
-
     const updateVehicle = () => {
         if (!canUptade) return;
         if (vehicleIndex === 9) return;
         setvehicleIndex(vehicleIndex+1);
         setCanUptade(false);
     }
+
+    const goUp = () => {
+        if(!canClick) return;
+        // go to the bottom of the page
+        window.scrollTo(0, document.body.scrollHeight);
+        setTimeout(() => {
+            setIsGoingUp(true);
+        }, 50);
+        
+        // show info message 
+    }
     
   return (
-    <div className='fixed bottom-0 right-0 mr-6 z-50 flex flex-col justify-center items-center'>
-
-            <div className={`rounded-full bg-white p-2 ${(canUptade) ? "border-green-500" : "border-gray-400"} border-4`}>
-
-                <Image 
-                    src={require(`@/assets/${vehicles[vehicleIndex].img}`)}
-                    alt="bicicross"
-                    className='w-[4.5rem] h-[4.5rem]'
-                />
-
-            </div>
+    <div>
 
             {
-                (canUptade && vehicleIndex !== 9) && <button onClick={updateVehicle} type='button' className='bg-green-500 py-2 px-3 text-white rounded-full -translate-y-8 text-lg'>Update</button>
+                isGoingUp &&    <div className='fixed w-full bottom-0 left-0 z-50'>
+
+                                    <Image 
+                                        src={require(`@/assets/${vehicles[vehicleIndex].img}`)}
+                                        alt="bicicross"
+                                        className='w-[12rem] h-[12rem] rotate-90 -translate-x-9'
+                                    />
+
+                                    <div className='bg-gradient-to-b from-transparent via-black to-black text-white flex items-center px-10 py-[5rem]'>
+                                        
+                                        <p className='text-lg'>You are currently on a {vehicles[vehicleIndex].name}. The maximum speed of this object is: {vehicles[vehicleIndex].velocity}. 
+                                                            Therefore it will take about: 20 days to rise to the top of the page.</p>
+                                    </div>
+
+                                </div>
+                
             }
 
             {
-                (!canUptade && vehicleIndex !== 9) && <p className='bg-gray-400 py-2 px-3 text-white rounded-full -translate-y-8 text-lg'>{counterTime}s</p>
+                !isGoingUp &&  <div ref={elemento} className={`fixed bottom-0 right-0 mr-6 z-40 flex flex-col justify-center items-center`}>
+                                        <button onClick={goUp}
+                                                type='button' 
+                                                className={`rounded-full bg-white p-2 ${(canUptade) ? "border-green-500" : "border-gray-400"} border-4`}>
+
+                                            <Image 
+                                                src={require(`@/assets/${vehicles[vehicleIndex].img}`)}
+                                                alt="bicicross"
+                                                className='w-[4.5rem] h-[4.5rem]'
+                                            />
+
+                                        </button>
+
+                                        {
+                                            (canUptade && vehicleIndex !== 9) && <button onClick={updateVehicle} type='button' className='bg-green-500 py-2 px-3 text-white rounded-full -translate-y-8 text-lg'>Update</button>
+                                        }
+
+                                        {
+                                            (!canUptade && vehicleIndex !== 9) && <p className='bg-gray-400 py-2 px-3 text-white rounded-full -translate-y-8 text-lg'>{counterTime}s</p>
+                                        }
+
+                                        {
+                                            (vehicleIndex === 9) && <p className='opacity-0'>{counterTime}s</p>
+                                        }
+                                        
+                                </div>
             }
 
-            {
-                (vehicleIndex === 9) && <p className='opacity-0'>{counterTime}s</p>
-            }
-            
+
     </div>
+
   )
 }
