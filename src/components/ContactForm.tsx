c22198e93adb363478d1ad1e7f2a8fa034f3bec4
A-N-D-R-E-S-B-O-N-I-L-L-A <andresbonilla1256@gmail.com>
2023-11-14 13:32:13 -0500

    validations finished in the form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,21 +3,32 @@ import React, { FormEvent, useState } from 'react'
 
 export const ContactForm = () => {
 
+  const [nameError, setNameError] = useState("")
+  const [emailError, setEmailError] = useState("")
+  const [messageError, setMessageError] = useState("")
+
   const sendEmail = (e:FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const messageInfo = {
-      name: formData.get('name') ?? '',
-      email: formData.get('email') ?? '',
-      message: formData.get('message') ?? '',
+      name: formData.get('name') ? formData.get("name") :'',
+      email: formData.get('email') ? formData.get("email") :'',
+      message: formData.get('message') ? formData.get("message") :'',
     }
-    if (messageInfo.name === "") {
+    if (messageInfo.name?.toString() === "") {
+      setNameError("The Name is required.");
       return;
     }
-    if (messageInfo.email === "") {
+    if (messageInfo.email?.toString() === "") {
+      setEmailError("The Email is required.");
       return;
     }
-    if (messageInfo.message === "") {
+    if (messageInfo.message?.toString() === "") {
+      setMessageError("The Email is required.");
+      return;
+    }
+    if (messageInfo.message?.toString() && messageInfo.message?.toString().length < 10) {
+      setMessageError("The message must have at least 10 chracters");
       return;
     }
   }
@@ -25,23 +36,44 @@ export const ContactForm = () => {
   return (
     <form onSubmit={sendEmail} className='w-full'> 
 
-        <input type="text" 
-               placeholder='Name'
-               name='name'
-               className='w-full mt-8 py-3 px-4 bg-[#008CFF] text-white'/>
+        <div>
 
-        <input type="text" 
-               placeholder='Email'
-               name='email'
-               className='w-full mt-8 py-3 px-4 bg-[#008CFF] text-white'/>
+            <input type="text" 
+                  placeholder='Name'
+                  name='name'
+                  className={`${(nameError !== "") ? "border-2 border-red-500" : ""} w-full mt-8 py-3 px-4 bg-[#008CFF] text-white`}
+            />
 
-        <textarea name="message" 
-                  id=""
-                  rows={"5" as any} 
-                  placeholder='Message'
-                  className='w-full mt-8 py-3 px-4 bg-[#008CFF] text-white'>
-            
-        </textarea>
+            {nameError && <p className='text-red-500 text-lg'>{nameError}</p>}
+
+        </div>
+        
+
+        <div>
+
+            <input type="text" 
+                  placeholder='Name'
+                  name='email'
+                  className={`${(emailError !== "") ? "border-2 border-red-500" : ""} w-full mt-8 py-3 px-4 bg-[#008CFF] text-white`}
+            />
+
+            {emailError && <p className='text-red-500 text-lg'>{emailError}</p>}
+
+        </div>
+
+        <div>
+
+          <textarea name="message" 
+                    id=""
+                    rows={"5" as any} 
+                    placeholder='Message'
+                    className={`${(emailError !== "") ? "border-2 border-red-500" : ""} w-full mt-8 py-3 px-4 bg-[#008CFF] text-white`}>
+              
+          </textarea>
+
+          {messageError && <p className='text-red-500 text-lg'>{messageError}</p>}
+
+        </div>
 
         <button type='submit' className='mt-8 w-full flex items-center justify-center bg-yellow-200 py-4 px-5 text-lg font-bold text-[#0F193B]'>Send</button>
 
