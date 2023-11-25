c31ceb4b565ada4287b67c7cf5287a4665e315a0
Andres Bonilla <andresbonilla1256@gmail.com>
2023-11-24 21:56:25 -0500

    lenguage added to the vehicle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     <>
       
       <Navbar lenguage={"en"}/>
-      <Vehicle />
+      <Vehicle lenguage={"en"}/>
       <main className='overflow-x-hidden z-20'>
           
           <div className='gradient-background'>
