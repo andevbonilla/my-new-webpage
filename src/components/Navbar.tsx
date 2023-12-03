b7dfa705b46200178d79c78c9a52461a6822024e
Andres Bonilla <andresbonilla1256@gmail.com>
2023-12-03 14:49:06 -0500

    small formatting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,13 @@ import { faBars, faGlobe, faXmark } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
 
 
-export const Navbar = ({lenguage}:any) => {
+export const Navbar = ({ lenguage }: any) => {
 
     const [asteroidsMenuOpen, setAsteroidsMenuOpen] = useState(false);
     const [showLenguageMenu, setshowLenguageMenu] = useState(false);
     const [activeClass, setactiveClass] = useState(false);
-    const navbar:any = useRef(null)
-    
+    const navbar: any = useRef(null)
+
     const router = useRouter();
 
     const [texts, setTexts] = useState({
@@ -40,23 +40,23 @@ export const Navbar = ({lenguage}:any) => {
         if (currentDisplacement === principalUbication) return;
 
 
-        if(currentDisplacement >= 1 ){
+        if (currentDisplacement >= 1) {
             setactiveClass(true);
-        }else{
+        } else {
             setactiveClass(false);
         };
         if (navbar.current !== null) {
 
             if (principalUbication >= currentDisplacement) {
                 navbar.current.style.top = '0';
-                navbar.current.style.transition = '.5s';            
+                navbar.current.style.transition = '.5s';
             } else {
                 navbar.current.style.top = '-400px';
                 navbar.current.style.transition = '1s';
             }
 
             principalUbication = currentDisplacement;
-                
+
         };
     };
 
@@ -72,7 +72,7 @@ export const Navbar = ({lenguage}:any) => {
                 aboutAsteroid: "ABOUT",
                 bussinessAsteroid: "BUSINESS",
             })
-        }else if(lenguage === "es"){
+        } else if (lenguage === "es") {
             setTexts({
                 svgUrl: "flag-for-spain-svgrepo-com.svg",
                 LenguageName: "ES",
@@ -82,7 +82,7 @@ export const Navbar = ({lenguage}:any) => {
                 aboutAsteroid: "SOBRE MI",
                 bussinessAsteroid: "NEGOCIOS",
             })
-        }else if(lenguage === "fr"){
+        } else if (lenguage === "fr") {
             setTexts({
                 svgUrl: "flag-for-spain-svgrepo-com.svg",
                 LenguageName: "FR",
@@ -92,7 +92,7 @@ export const Navbar = ({lenguage}:any) => {
                 aboutAsteroid: "SUR MI",
                 bussinessAsteroid: "ENTREPRISE",
             })
-        }else if(lenguage === "ch"){
+        } else if (lenguage === "ch") {
             setTexts({
                 svgUrl: "flag-for-spain-svgrepo-com.svg",
                 LenguageName: "CH",
@@ -102,7 +102,7 @@ export const Navbar = ({lenguage}:any) => {
                 aboutAsteroid: "关于我",
                 bussinessAsteroid: "商业",
             })
-        }else if(lenguage === "hi"){
+        } else if (lenguage === "hi") {
             setTexts({
                 svgUrl: "flag-for-spain-svgrepo-com.svg",
                 LenguageName: "HI",
@@ -112,7 +112,7 @@ export const Navbar = ({lenguage}:any) => {
                 aboutAsteroid: "मेरे बारे में",
                 bussinessAsteroid: "व्यापार",
             })
-        }else if(lenguage === "ja"){
+        } else if (lenguage === "ja") {
             setTexts({
                 svgUrl: "flag-for-spain-svgrepo-com.svg",
                 LenguageName: "JA",
@@ -122,7 +122,7 @@ export const Navbar = ({lenguage}:any) => {
                 aboutAsteroid: "私について",
                 bussinessAsteroid: "仕事",
             })
-        }else {
+        } else {
             setTexts({
                 svgUrl: "flag-for-united-kingdom-svgrepo-com.svg",
                 LenguageName: "EN",
@@ -141,7 +141,7 @@ export const Navbar = ({lenguage}:any) => {
 
     }, []);
 
-    
+
     const openAsteroidsMenu = () => {
         setAsteroidsMenuOpen(true);
     };
@@ -150,7 +150,7 @@ export const Navbar = ({lenguage}:any) => {
         setAsteroidsMenuOpen(false);
     };
 
-    const clickOnAsteroid = (link:string) => {
+    const clickOnAsteroid = (link: string) => {
         setAsteroidsMenuOpen(false);
         router.push(link);
     };
@@ -164,142 +164,142 @@ export const Navbar = ({lenguage}:any) => {
     };
 
 
-  return (
-    <>
+    return (
+        <>
 
-        <nav ref={navbar} className={`${activeClass ? "bg-gradient-to-b from-black to-transparent" : ""} z-[998] fixed top-0 left-0 w-full flex justify-between items-center py-10 px-[15%]`}>
+            <nav ref={navbar} className={`${activeClass ? "bg-gradient-to-b from-black to-transparent" : ""} z-[998] fixed top-0 left-0 w-full flex justify-between items-center py-10 px-[15%]`}>
 
-            <Image 
-                src={require("@/assets/slogan.png")}
-                alt='Andres Bonilla Logo'
-                width={60}
-                height={60}
-            />
-
-            <div className='flex items-center'>
-
-                <button onClick={openLenguagesMenu} type='button' className='flex items-center mr-3 lg:mr-6 cursor-pointer'>
-                    <FontAwesomeIcon icon={faGlobe} size='2x' className='text-white'/>
-                    <p className='text-white font-bold ml-3'>{texts.LenguageName}</p>
-                </button>
-
-                <FontAwesomeIcon
-                    onClick={openAsteroidsMenu} 
-                    icon={faBars}
-                    className='text-white cursor-pointer'
-                    size='2x' 
+                <Image
+                    src={require("@/assets/slogan.png")}
+                    alt='Andres Bonilla Logo'
+                    width={60}
+                    height={60}
                 />
-            </div>
 
-        </nav>
+                <div className='flex items-center'>
 
-        {/* change the lenguage part */}
-        {
-            showLenguageMenu && <div className='flex justify-center items-center h-screen fixed w-full top-0 left-0 z-[999]'>
-                                   <div onClick={closeLenguagesMenu} className='cursor-pointer z-[1000] fixed top-0 left-0 w-full h-screen bg-black bg-opacity-50'></div>
-                                   <div className='bg-white py-10 px-12 rounded z-[1001]'>
+                    <button onClick={openLenguagesMenu} type='button' className='flex items-center mr-3 lg:mr-6 cursor-pointer'>
+                        <FontAwesomeIcon icon={faGlobe} size='2x' className='text-white' />
+                        <p className='text-white font-bold ml-3'>{texts.LenguageName}</p>
+                    </button>
 
-                                        <h2 className='font-bold text-xl mb-6'>{texts.changeLenguageText}</h2>
+                    <FontAwesomeIcon
+                        onClick={openAsteroidsMenu}
+                        icon={faBars}
+                        className='text-white cursor-pointer'
+                        size='2x'
+                    />
+                </div>
 
-                                        <Link href={"/"} className='text-black flex items-center mb-2 hover:bg-slate-300 hover:transition-all p-3 rounded'>
-                                            <Image 
-                                                src={require("@/assets/flag-for-united-kingdom-svgrepo-com.svg")}
-                                                alt='spain flag'
-                                                width={40}
-                                                height={40}
-                                            />
-                                            <p className='ml-2 text-xl'>English</p>
-                                        </Link>
+            </nav>
 
-                                        <Link href={"/es"} className='text-black flex items-center mb-2 hover:bg-slate-300 hover:transition-all p-3 rounded'>
-                                            <Image 
-                                                src={require("@/assets/flag-for-spain-svgrepo-com.svg")}
-                                                alt='spain flag'
-                                                width={40}
-                                                height={40}
-                                            />
-                                            <p className='ml-2 text-xl'>Español</p>
-                                        </Link>
+            {/* change the lenguage part */}
+            {
+                showLenguageMenu && <div className='flex justify-center items-center h-screen fixed w-full top-0 left-0 z-[999]'>
+                    <div onClick={closeLenguagesMenu} className='cursor-pointer z-[1000] fixed top-0 left-0 w-full h-screen bg-black bg-opacity-50'></div>
+                    <div className='bg-white py-10 px-12 rounded z-[1001]'>
 
-                                        <Link href={"/ja"} className='text-black flex items-center mb-2 hover:bg-slate-300 hover:transition-all p-3 rounded'>
-                                            <Image 
-                                                src={require("@/assets/flag-for-flag-japan-svgrepo-com.svg")}
-                                                alt='spain flag'
-                                                width={40}
-                                                height={40}
-                                            />
-                                            <p className='ml-2 text-xl'>日本語</p>
-                                        </Link>
+                        <h2 className='font-bold text-xl mb-6'>{texts.changeLenguageText}</h2>
 
-                                        <Link href={"/fr"} className='text-black flex items-center mb-2 hover:bg-slate-300 hover:transition-all p-3 rounded'>
-                                            <Image 
-                                                src={require("@/assets/flag-for-flag-st-martin-svgrepo-com.svg")}
-                                                alt='spain flag'
-                                                width={40}
-                                                height={40}
-                                            />
-                                            <p className='ml-2 text-xl'>Français</p>
-                                        </Link>
+                        <Link href={"/"} className='text-black flex items-center mb-2 hover:bg-slate-300 hover:transition-all p-3 rounded'>
+                            <Image
+                                src={require("@/assets/flag-for-united-kingdom-svgrepo-com.svg")}
+                                alt='spain flag'
+                                width={40}
+                                height={40}
+                            />
+                            <p className='ml-2 text-xl'>English</p>
+                        </Link>
 
-                                        <Link href={"/ch"} className='text-black flex items-center mb-2 hover:bg-slate-300 hover:transition-all p-3 rounded'>
-                                            <Image 
-                                                src={require("@/assets/flag-for-flag-china-svgrepo-com.svg")}
-                                                alt='spain flag'
-                                                width={40}
-                                                height={40}
-                                            />
-                                            <p className='ml-2 text-xl'>中文 {"("}简体{")"}</p>
-                                        </Link>
+                        <Link href={"/es"} className='text-black flex items-center mb-2 hover:bg-slate-300 hover:transition-all p-3 rounded'>
+                            <Image
+                                src={require("@/assets/flag-for-spain-svgrepo-com.svg")}
+                                alt='spain flag'
+                                width={40}
+                                height={40}
+                            />
+                            <p className='ml-2 text-xl'>Español</p>
+                        </Link>
 
-                                        <Link href={"/hi"} className='text-black flex items-center mb-2 hover:bg-slate-300 hover:transition-all p-3 rounded'>
-                                            <Image 
-                                                src={require("@/assets/flag-for-flag-india-svgrepo-com.svg")}
-                                                alt='spain flag'
-                                                width={40}
-                                                height={40}
-                                            />
-                                            <p className='ml-2 text-xl'>हिंदी</p>
-                                        </Link>
-                                        
-                                    </div>
-                                </div>
-            
-        }
+                        <Link href={"/ja"} className='text-black flex items-center mb-2 hover:bg-slate-300 hover:transition-all p-3 rounded'>
+                            <Image
+                                src={require("@/assets/flag-for-flag-japan-svgrepo-com.svg")}
+                                alt='spain flag'
+                                width={40}
+                                height={40}
+                            />
+                            <p className='ml-2 text-xl'>日本語</p>
+                        </Link>
 
-        {
-            asteroidsMenuOpen && <div className='fixed w-full h-screen z-[999] bg-[#0F193B]'>
-                                    <div className='stars'>
-                                        <div className='flex justify-end items-center p-10'>
-                                            <FontAwesomeIcon
-                                                    onClick={closeAsteroidsMenu} 
-                                                    icon={faXmark}
-                                                    className='text-white w-12 h-12 cursor-pointer' 
-                                            />
-                                        </div>
-                                        <div className='overflow-hidden'>
+                        <Link href={"/fr"} className='text-black flex items-center mb-2 hover:bg-slate-300 hover:transition-all p-3 rounded'>
+                            <Image
+                                src={require("@/assets/flag-for-flag-st-martin-svgrepo-com.svg")}
+                                alt='spain flag'
+                                width={40}
+                                height={40}
+                            />
+                            <p className='ml-2 text-xl'>Français</p>
+                        </Link>
 
-                                            <button onClick={()=>clickOnAsteroid("/#about")} className='w-[7rem] h-[7rem] flex justify-center items-center mb-8 asteroid' id='asteroid'>
-                                                <p className='font-bold text-white text-2xl'>{texts.aboutAsteroid}</p>
-                                            </button>
+                        <Link href={"/ch"} className='text-black flex items-center mb-2 hover:bg-slate-300 hover:transition-all p-3 rounded'>
+                            <Image
+                                src={require("@/assets/flag-for-flag-china-svgrepo-com.svg")}
+                                alt='spain flag'
+                                width={40}
+                                height={40}
+                            />
+                            <p className='ml-2 text-xl'>中文 {"("}简体{")"}</p>
+                        </Link>
 
-                                            <button onClick={()=>clickOnAsteroid("/#business-part")} className='w-[10rem] h-[10rem] flex justify-center items-center mb-12 asteroid1' id='asteroid1'>
-                                                <p className='font-bold text-white text-2xl'>{texts.bussinessAsteroid}</p>                         
-                                            </button>
+                        <Link href={"/hi"} className='text-black flex items-center mb-2 hover:bg-slate-300 hover:transition-all p-3 rounded'>
+                            <Image
+                                src={require("@/assets/flag-for-flag-india-svgrepo-com.svg")}
+                                alt='spain flag'
+                                width={40}
+                                height={40}
+                            />
+                            <p className='ml-2 text-xl'>हिंदी</p>
+                        </Link>
 
-                                            <button onClick={()=>clickOnAsteroid("/#projects")} className='w-[10rem] h-[10rem] flex justify-center items-center mb-12 asteroid1' id='asteroid2'>
-                                                <p className='font-bold text-white text-2xl'>{texts.projectAsteroid}</p>                         
-                                            </button>
+                    </div>
+                </div>
 
-                                            <button onClick={()=>clickOnAsteroid("/#contact")} className='w-[8rem] h-[8rem] flex justify-center items-center mb-12 asteroid3' id='asteroid3'>
-                                                <p className='font-bold text-white text-2xl'>{texts.contactAsteroid}</p>
-                                            </button>
+            }
 
-                                        </div>
-                                    </div>
-                                 </div>
-        }
-        
-    
-    </>
-  )
+            {
+                asteroidsMenuOpen && <div className='fixed w-full h-screen z-[999] bg-[#0F193B]'>
+                    <div className='stars'>
+                        <div className='flex justify-end items-center p-10'>
+                            <FontAwesomeIcon
+                                onClick={closeAsteroidsMenu}
+                                icon={faXmark}
+                                className='text-white w-12 h-12 cursor-pointer'
+                            />
+                        </div>
+                        <div className='overflow-hidden'>
+
+                            <button onClick={() => clickOnAsteroid("/#about")} className='w-[7rem] h-[7rem] flex justify-center items-center mb-8 asteroid' id='asteroid'>
+                                <p className='font-bold text-white text-2xl'>{texts.aboutAsteroid}</p>
+                            </button>
+
+                            <button onClick={() => clickOnAsteroid("/#business-part")} className='w-[10rem] h-[10rem] flex justify-center items-center mb-12 asteroid1' id='asteroid1'>
+                                <p className='font-bold text-white text-2xl'>{texts.bussinessAsteroid}</p>
+                            </button>
+
+                            <button onClick={() => clickOnAsteroid("/#projects")} className='w-[10rem] h-[10rem] flex justify-center items-center mb-12 asteroid1' id='asteroid2'>
+                                <p className='font-bold text-white text-2xl'>{texts.projectAsteroid}</p>
+                            </button>
+
+                            <button onClick={() => clickOnAsteroid("/#contact")} className='w-[8rem] h-[8rem] flex justify-center items-center mb-12 asteroid3' id='asteroid3'>
+                                <p className='font-bold text-white text-2xl'>{texts.contactAsteroid}</p>
+                            </button>
+
+                        </div>
+                    </div>
+                </div>
+            }
+
+
+        </>
+    )
 }
